17da0e7a9e2a8c71a7f3e0b0e503f1f668801fa0
gregor <info@gregorkofler.com>
2014-05-12 21:40:30 +0200

    Login screen redesigned; minor updates to CSS
    Change-Id: I8166aec1e1b08a25a05dff15d52e6e5bbbba79ab

--- view/admin/login.php
+++ view/admin/login.php
@@ -6,10 +6,12 @@
 	vxJS.event.addDomReadyListener(this.vxWeb.doLogin);
 </script>
 
-<div id="adminLogin">
-	<p style="text-align: right; color: #808080; font-size: 0.7em; padding: 0em 8px; margin: 0;">realisiert mit vxPHP/vxJS, &copy;2007- <?php echo date('Y'); ?> <a href="http://vxweb.net">vxWEB</a></p>
-	<?php echo $tpl->form; ?>
-	<p style="text-align: right; color: #808080; font-size: 0.7em; padding: 0 8px; margin: 0;">Zurück zu <a href="/"> <strong><?php echo $_SERVER['HTTP_HOST']; ?></strong></a></p>
+<div id="wrapper" style="margin-top: 10em; padding: 2em 0; background: #9df; box-shadow: 0 0 1em #444 inset; border-top: solid 1px #aef; border-bottom: solid 1px #aef;">
+	<div id="adminLogin">
+		<p style="text-align: right; font-size: 80%; padding: 0em 0.5em; margin: 0;">realisiert mit <a href="http://vxweb.net">vxWeb</a> &copy;2007- <?php echo date('Y'); ?></p>
+		<?php echo $tpl->form; ?>
+		<p style="text-align: right; font-size: 80%; padding: 0 0.5em; margin: 0;">Zurück zu <a href="/"><?php echo $_SERVER['HTTP_HOST']; ?></a></p>
+	</div>
 </div>
 
 <div id="messageBox">&nbsp;</div>
