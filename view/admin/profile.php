fcc31b014bea111a00649e6374360f9aff930139
gregor <info@gregorkofler.com>
2014-05-15 20:22:21 +0200

    Improved CSS; only dropdown selects are styled
    Change-Id: I42514a293c0b25f142b529dc741cf4bb3f1ad46e

--- view/admin/profile.php
+++ view/admin/profile.php
@@ -13,3 +13,12 @@
 <div class="inputGroup xl" style="margin-bottom: 1em"><span class="s">Ok</span><input class="pct_100" name="xxx" placeholder="test"><span class="s">here</span></div>
 <div class="inputGroup xl" style="margin-bottom: 1em"><input class="pct_100" name="xxx" placeholder="test"><span class="inputGroupBtn"><button>Ok</button></span></div>
 <div class="inputGroup xl" style="margin-bottom: 1em"><input class="pct_100" name="xxx" placeholder="test"><span class="inputGroupBtn"><button class="withIcon" data-icon="&#xe02e;">Login</button></span></div>
+<select size="3">
+	<option>1</option>
+	<option>2</option>
+</select>
+
+<select>
+	<option>1</option>
+	<option>2</option>
+</select>
