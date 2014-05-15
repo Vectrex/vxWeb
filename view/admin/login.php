31e65f638dac585d12688a32e9901f238e9f80ff
gregor <info@gregorkofler.com>
2014-05-15 17:13:33 +0200

    Minor CSS updates
    Change-Id: I24353c180939d927adfd80ff0b6127dbb7e89527

--- view/admin/login.php
+++ view/admin/login.php
@@ -6,7 +6,7 @@
 	vxJS.event.addDomReadyListener(this.vxWeb.doLogin);
 </script>
 
-<div id="wrapper" style="margin-top: 10em; padding: 2em 0; background: #9df; box-shadow: 0 0 1em #444 inset; border-top: solid 1px #aef; border-bottom: solid 1px #aef;">
+<div id="wrapper" style="margin-top: 8em; padding: 2em 0; background: #eee; box-shadow: 0 0 1em #444 inset; border-top: solid 1px #aef; border-bottom: solid 1px #aef;">
 	<div id="adminLogin">
 		<p style="text-align: right; font-size: 80%; padding: 0em 0.5em; margin: 0;">realisiert mit <a href="http://vxweb.net">vxWeb</a> &copy;2007- <?php echo date('Y'); ?></p>
 		<?php echo $tpl->form; ?>
