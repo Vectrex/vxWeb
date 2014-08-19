cfe50a294cff5f9eabc996f534d8e7854cecfdaf
gregor <info@gregorkofler.com>
2014-08-19 23:47:57 +0200

    Updated links to vxJS library files to match new file structure
    Change-Id: If99ac4113df31899dcdab2c9374067ba1e2de8bb

--- view/admin/layout_without_menu.php
+++ view/admin/layout_without_menu.php
@@ -15,17 +15,17 @@
 		<link type='text/css' rel='stylesheet'	href='/css/admin.css'>
 		<link type='text/css' rel='stylesheet'	href='/css/admin_site_specific.css'>
 
-		<script type='text/javascript' src='/js/core.js'></script>
-		<script type='text/javascript' src='/js/widget.js'></script>
-		<script type='text/javascript' src='/js/xhr.js'></script>
-		<script type='text/javascript' src='/js/extra/fx.js'></script>
-		<script type='text/javascript' src='/js/extra/dnd.js'></script>
-		<script type='text/javascript' src='/js/widgets/xhrform.js'></script>
-		<script type='text/javascript' src='/js/widgets/confirm.js'></script>
-		<script type='text/javascript' src='/js/widgets/calendar.js'></script>
-		<script type='text/javascript' src='/js/widgets/sortable.js'></script>
-		<script type='text/javascript' src='/js/widgets/tree.js'></script>
-		<script type='text/javascript' src='/js/widgets/simpletabs.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/core.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widget.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/xhr.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/extra/fx.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/extra/dnd.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widgets/xhrform.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widgets/confirm.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widgets/calendar.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widgets/sortable.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widgets/tree.js'></script>
+		<script type='text/javascript' src='/js/vendor/vxJS/widgets/simpletabs.js'></script>
 
 	</head>
 
