a2c446fa9cef3cabec6863866f01f423171a4e48
gregor <info@gregorkofler.com>
2018-04-10 12:36:12 +0200

    Bruteforce throttler can observe whitelisted ips

--- src/Service/vxWeb/BruteforceThrottler.php
+++ src/Service/vxWeb/BruteforceThrottler.php
@@ -3,12 +3,34 @@
 namespace App\Service\vxWeb;
 
 
+use vxPHP\Constraint\Validator\Ip;
 use vxPHP\Service\ServiceInterface;
 
 class BruteforceThrottler implements ServiceInterface
 {
 
+    /**
+     * @var array
+     */
+    protected $whiteListedIps = [];
+
     public function setParameters(array $parameters) {
+
+        if(isset($parameters['whitelisted_ips'])) {
+
+            $validator = new Ip();
+
+            foreach(explode(',', $parameters['whitelisted_ips']) as $ip) {
+
+                if(!$validator->validate(trim($ip))) {
+                    throw new \InvalidArgumentException(sprintf("Invalid ip '%s'", $ip));
+                }
+
+                $this->whiteListedIps[] = trim($ip);
+            }
+
+        }
+
     }
 
     public function throttle($ip) {
