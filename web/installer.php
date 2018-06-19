661ed7abd98761f861efe47773a8fc4bf007c431
Gregor Kofler <gregor@han.agindo.intern>
2018-06-19 15:44:47 +0200

    Installer no longer requires a writable ini directory; fixes issue #11

--- web/installer.php
+++ web/installer.php
@@ -1,26 +1,62 @@
 <?php
+// assume that script resides in "public" subdir
 
-$assetsPath		= getcwd();
-$rootPath		= $assetsPath;
+$assetsPath = getcwd();
+$rootPath = dirname($assetsPath);
 
-if(file_exists($rootPath . DIRECTORY_SEPARATOR . 'bootstrap.php')) {
-	require 'bootstrap.php';
+// package was installed with composer?
+
+if(file_exists($rootPath . '/vendor/autoload.php')) {
+	require_once $rootPath . '/vendor/autoload.php';
 }
+
+// if not set up own autoloaders
 
 else {
 
-	$rootPath = dirname($rootPath);
-
-	if(file_exists($rootPath . DIRECTORY_SEPARATOR . 'bootstrap.php')) {
-		require $rootPath . DIRECTORY_SEPARATOR . 'bootstrap.php';
-	}
+	require_once $rootPath . '/vendor/vxPHP/Autoload/Psr4.php';
 	
-	else {
-		die('Cannot load bootstrap code from "' . $rootPath . DIRECTORY_SEPARATOR . 'bootstrap.php".');
-	}
+	$loader = new vxPHP\Autoload\Psr4();
+	$loader->register();
+	
+	$loader->addPrefix('vxPHP', $rootPath . '/vendor/vxPHP');
+
 }
 
-$app = \vxPHP\Application\Application::getInstance();
+$iniPath = $rootPath . DIRECTORY_SEPARATOR . 'ini' . DIRECTORY_SEPARATOR;
+$configFilename = $iniPath . 'site.ini.xml';
+
+// read configuration and cache if necessary
+
+if(!file_exists($configFilename)) {
+	die ('No site.ini.xml in ' . $iniPath . ' found.');
+}
+
+// create config instance
+
+try {
+	$config = new vxPHP\Application\Config($configFilename);
+}
+catch(\vxPHP\Application\Exception\ConfigException $e) {
+	die('<h1>Cannot create Config instance</h1><pre>' . $e->getMessage() . '</pre>');
+}
+
+// initialize application
+
+$app = vxPHP\Application\Application::getInstance($config);
+
+$app
+	->setRootPath			($rootPath)
+	->setAbsoluteAssetsPath	($assetsPath)
+;
+
+// set debugging and error reporting level depending on environment
+
+if(!$app->runsLocally()) {
+	vxPHP\Debug\Debug::enable(E_ALL, FALSE);
+}
+
+// without composer install add application namespace for autoloader
 
 if(isset($loader)) {
     $loader->addPrefix(
