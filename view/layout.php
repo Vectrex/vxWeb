3f72c00a5a3abcc770586761ea7ee4b64e2eb362
Gregor Kofler <info@gregorkofler.com>
2018-12-06 11:21:58 +0100

    Meta tags in default layout updated

--- view/layout.php
+++ view/layout.php
@@ -3,9 +3,10 @@
 <html>
 	<head>
 		<title>vxWeb - Layout Template</title>
-		<meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-		<meta name='author' content='Gregor Kofler - Mediendesign und Webapplikationen, http://gregorkofler.com'>
-		<link type='text/css' rel='stylesheet' href='/css/default.css'>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name='web-author' content="Gregor Kofler - Mediendesign und Webapplikationen, http://gregorkofler.com">
+        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+		<link type="text/css" rel="stylesheet" href="/css/default.css">
 	</head>
 
 	<body>
