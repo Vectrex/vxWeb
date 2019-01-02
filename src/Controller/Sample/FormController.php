7c9493cd9712bad169dc36f52625e51f08566752
Gregor Kofler <info@gregorkofler.com>
2019-01-03 00:09:26 +0100

    Added a sample configuration, sample controller and sample form; simplified layout file

---
+++ src/Controller/Sample/FormController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Sample;
+
+use vxPHP\Controller\Controller;
+use vxPHP\Form\FormElement\ButtonElement;
+use vxPHP\Form\FormElement\CheckboxElement;
+use vxPHP\Form\FormElement\FormElementWithOptions\RadioElement;
+use vxPHP\Form\FormElement\FormElementWithOptions\SelectElement;
+use vxPHP\Form\FormElement\InputElement;
+use vxPHP\Form\FormElement\LabelElement;
+use vxPHP\Form\HtmlForm;
+use vxPHP\Http\Response;
+use vxPHP\Template\SimpleTemplate;
+
+class FormController extends Controller
+{
+
+    protected function execute()
+    {
+
+        $input = (new InputElement('input_element'))->setLabel(new LabelElement('An input element'));
+        $checkbox = (new CheckboxElement('checkbox_element'))->setLabel(new LabelElement('A checkbox to click'));
+        $select = (new SelectElement('select_element'))->createOptions(['-1' => 'pick an animal...', 'dog' => 'Dog', 'cat' => 'Cat'])->setLabel(new LabelElement('A select element'));
+        $radio = (new RadioElement('radio_element'))->createOptions(['n' => 'no', 'nn' => 'nope', 'nnn' => 'never']);
+        $button = (new ButtonElement('submit', null, 'submit'))->setInnerHTML('submit form');
+
+        $form = new HtmlForm('sample/form.htm');
+
+        $form
+            ->addElement($input)
+            ->addElement($checkbox)
+            ->addElement($select)
+            ->addElement($radio)
+            ->addElement($button);
+
+        $inputs = [];
+
+        for ($i = 1; $i <= 3; ++$i) {
+            $inputs[] = (new InputElement('input_array'))->setLabel(new LabelElement('input array ' . $i));
+        }
+
+        $form->addElementArray($inputs);
+
+        $formTpl = (new SimpleTemplate())->setRawContents($form->render());
+        $tpl = new SimpleTemplate('layout.php');
+        $tpl->insertTemplateAt($formTpl, 'content_block');
+
+        return new Response($tpl->display());
+
+
+    }
+}
--- view/layout.php
+++ view/layout.php
@@ -6,14 +6,13 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 		<meta name='web-author' content="Gregor Kofler - Mediendesign und Webapplikationen, http://gregorkofler.com">
         <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
-		<link type="text/css" rel="stylesheet" href="/css/default.css">
+		<link type="text/css" rel="stylesheet" href="css/site.min.css">
 	</head>
 
 	<body>
 		<div id="page">
 
 			<div id="main">
-				<?php echo vxPHP\Webpage\MenuGenerator::create('main', 0)->render(); ?>
 
 				<div id="content">
 				<!-- { block: content_block } -->
