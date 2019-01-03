ddbacf81ce9544385b7711e4a7b2e54ac526ea94
Gregor Kofler <info@gregorkofler.com>
2019-01-03 23:31:55 +0100

    Extended sample form

--- src/Controller/Sample/FormController.php
+++ src/Controller/Sample/FormController.php
@@ -9,6 +9,7 @@ use vxPHP\Form\FormElement\FormElementWithOptions\RadioElement;
 use vxPHP\Form\FormElement\FormElementWithOptions\SelectElement;
 use vxPHP\Form\FormElement\InputElement;
 use vxPHP\Form\FormElement\LabelElement;
+use vxPHP\Form\FormElement\TextareaElement;
 use vxPHP\Form\HtmlForm;
 use vxPHP\Http\Response;
 use vxPHP\Template\SimpleTemplate;
@@ -20,10 +21,11 @@ class FormController extends Controller
     {
 
         $input = (new InputElement('input_element'))->setLabel(new LabelElement('An input element'));
-        $checkbox = (new CheckboxElement('checkbox_element'))->setLabel(new LabelElement('A checkbox to click'));
+        $checkbox = (new CheckboxElement('checkbox_element'))->setLabel(new LabelElement('A checkbox to click'))->setAttribute('id', 'form-checkbox');
         $select = (new SelectElement('select_element'))->createOptions(['-1' => 'pick an animal...', 'dog' => 'Dog', 'cat' => 'Cat'])->setLabel(new LabelElement('A select element'));
         $radio = (new RadioElement('radio_element'))->createOptions(['n' => 'no', 'nn' => 'nope', 'nnn' => 'never']);
         $button = (new ButtonElement('submit', null, 'submit'))->setInnerHTML('submit form');
+        $textarea = (new TextareaElement('textarea_element'))->setLabel(new LabelElement('A textarea element'));
 
         $form = new HtmlForm('sample/form.htm');
 
@@ -32,7 +34,9 @@ class FormController extends Controller
             ->addElement($checkbox)
             ->addElement($select)
             ->addElement($radio)
-            ->addElement($button);
+            ->addElement($textarea)
+            ->addElement($button)
+        ;
 
         $inputs = [];
 
