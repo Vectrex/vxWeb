f414ec508ff7f134ffb5e3502984044e31a8a944
Gregor Kofler <info@gregorkofler.com>
2019-03-03 22:27:29 +0100

    further refinement of vuejs users list

--- view/admin/users_list.php
+++ view/admin/users_list.php
@@ -5,6 +5,7 @@
 <h1>User</h1>
 
 <script type="text/javascript">
+    /*
 	vxJS.event.addDomReadyListener(function() {
 		var lsValue, lsKey = window.location.origin + "/admin/users__sort__",
 			t = vxJS.widget.sorTable(
@@ -34,7 +35,7 @@
             }
         }
 	});
-
+*/
 </script>
 
 <?php $currentUsername = vxPHP\Application\Application::getInstance()->getCurrentUser()->getUsername(); ?>
@@ -44,69 +45,91 @@
 </div>
 
 <table class="table table-striped">
-	<tr>
-		<th class="col-3 vx-sortable-header" @click="sort('username')">Username</th>
-		<th class="col-2 vx-sortable-header" @click="sort('name')">Name</th>
-		<th>Email</th>
-		<th class="col-2 vx-sortable-header" @click="sort('alias')">Gruppe</th>
-		<th class="col-1"></th>
-	</tr>
+    <thead>
+        <tr>
+            <th  v-for="column in columns" class="vx-sortable-header" :class="[ setHeaderClass(column), columnProperties[column].width ? columnProperties[column].width : '' ]" @click="sort(column)">{{ columnProperties[column].label }}</th>
+            <th class="col-1"></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="user in users" :class="{ 'disabled' : currentUser.username === user.username }">
 
-    <tr v-for="user in users" v-bind:class="{ 'disabled' : currentUser.username === user.username }">
-
-        <td>{{ user.username }}</td>
-        <td>{{ user.name }}</td>
-        <td>{{ user.email }}</td>
-        <td>{{ user.alias }}</td>
-        <td class="right">
-                <a v-if="currentUser.username !== user.username" class="btn webfont-icon-only tooltip" data-tooltip="Bearbeiten" v-bind:href="'users?id=' + user.username">&#xe002;</a>
-                <a v-if="currentUser.username !== user.username" class="btn webfont-icon-only tooltip tooltip-left" data-tooltip="Löschen" v-bind:href="'users/del?id=' + user.username" onclick="return window.confirm('Wirklich löschen?');">&#xe011;</a>
-        </td>
-
-    </tr>
+            <td v-for="column in columns" :class="{ 'active': currentSortColumn === column }">{{ user[column] }}</td>
+            <td class="right">
+                <a v-if="currentUser.username !== user.username" class="btn webfont-icon-only tooltip" data-tooltip="Bearbeiten" :href="'users?id=' + user.username">&#xe002;</a>
+                <a v-if="currentUser.username !== user.username" class="btn webfont-icon-only tooltip tooltip-left" data-tooltip="Löschen" :href="'users/del?id=' + user.username" onclick="return window.confirm('Wirklich löschen?');">&#xe011;</a>
+            </td>
+        </tr>
+    </tbody>
 
 </table>
 
 <script>
+
     "use strict";
 
-    document.addEventListener("DOMContentLoaded", function() {
+    var app = new Vue({
 
-        var app = new Vue({
+        el: "table",
 
-            el: "table",
+        data: {
+            users: JSON.parse('<?= json_encode(array_map(function ($u) {
+                unset ($u['pwd']);
+                return $u;
+            }, (array)$this->users)) ?>'),
 
-            data: {
-                users: JSON.parse('<?= json_encode(array_map(function ($u) {
-                    unset ($u['pwd']);
-                    return $u;
-                }, (array)$this->users)) ?>'),
-
-                currentUser: {
-                    username: <?= json_encode(vxPHP\Application\Application::getInstance()->getCurrentUser()->getUsername()) ?>
-                },
-                currentSortDir: "asc"
+            currentUser: {
+                username: <?= json_encode(vxPHP\Application\Application::getInstance()->getCurrentUser()->getUsername()) ?>
             },
 
-            methods: {
-                sort: function(prop) {
+            columns: ["username", "name", "email", "alias"],
+
+            columnProperties: {
+                username: {label: "Username", sortable: true, width: "col-3" },
+                name: { label: "Name", sortable: true, width: "col-2" },
+                email: { label: "Email", sortable: false },
+                alias: { label: "Gruppe", sortable: true, width: "col-2" }
+            },
+
+            currentSortDir: "asc",
+            currentSortColumn: null
+        },
+
+        computed: {
+            fullname: function() { return alias + " " }
+        },
+
+        methods: {
+            setHeaderClass: function(column) {
+                if(this.currentSortColumn === column) {
+                    return this.currentSortDir;
+                }
+                return "";
+            },
+
+            sort: function(prop) {
+
+                if(this.columnProperties[prop].sortable) {
 
                     this.users.sort((a, b) => {
 
                         if (a[prop] < b[prop]) {
-                            return this.currentSortDir === "asc" ? 1 : -1;
-                        }
-                        if (a[prop] > b[prop]){
                             return this.currentSortDir === "asc" ? -1 : 1;
+                        }
+                        if (a[prop] > b[prop]) {
+                            return this.currentSortDir === "asc" ? 1 : -1;
                         }
 
                         return 0;
                     });
 
                     this.currentSortDir = this.currentSortDir === "asc" ? "desc" : "asc";
+                    this.currentSortColumn = prop;
 
                 }
+
             }
-        });
+        }
+
     });
 </script>
