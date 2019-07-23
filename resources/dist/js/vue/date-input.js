07745a4bb1347b64194e85c7ba83c7f7b634de98
Gregor Kofler <info@gregorkofler.com>
2019-07-23 16:39:53 +0200

    Additional work on date string parsing

--- resources/dist/js/vue/date-input.js
+++ resources/dist/js/vue/date-input.js
@@ -45,7 +45,6 @@
         methods: {
 
             formatDate(date, format) {
-
                 if (!date instanceof Date) {
                     return "";
                 }
@@ -59,6 +58,61 @@
                     .replace("%y", date.getFullYear().toString().slice(-2))
                     .replace("%Y", date.getFullYear())
                     .replace("%w", this.dayNames[date.getDay()].trim());
+            },
+
+            parseDate(dateString, format) {
+
+                let matches, escapedFormat = format.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), posMap = [];
+                const date = new Date();
+
+                // check for single day, month and year expression
+
+                if((matches = format.match(/\bd\b/gi)) && 1 === matches.length) {
+                    escapedFormat = escapedFormat.replace('d', '(\\d{1,2})');
+                }
+                else if((matches = format.match(/\bdd\b/gi)) && 1 === matches.length) {
+                    escapedFormat = escapedFormat.replace('dd', '(\\d{2})');
+                }
+                else {
+                    return false;
+                }
+                posMap[2] = format.toLowerCase().indexOf('d');
+
+                if((matches = format.match(/\bm\b/gi)) && 1 === matches.length) {
+                    escapedFormat = escapedFormat.replace('m', '(\\d{1,2})');
+                }
+                else if((matches = format.match(/\bmm\b/gi)) && 1 === matches.length) {
+                    escapedFormat = escapedFormat.replace('mm', '(\\d{2})');
+                }
+                else {
+                    return false;
+                }
+                posMap[1] = format.toLowerCase().indexOf('m');
+
+                if((matches = format.match(/\by\b/gi)) && 1 === matches.length) {
+                    escapedFormat = escapedFormat.replace('y', '(\\d{4})');
+                }
+                else {
+                    return false;
+                }
+                posMap[0] = format.toLowerCase().indexOf('y');
+
+                if(!(matches = dateString.match(escapedFormat))) {
+                    return false;
+                }
+
+                // remove first match
+
+                matches.shift();
+
+                /*
+                 e.g.
+                 [y, m, d] -
+                 [4, 0, 2] => [2, 0, 1] (m/d/y)
+                 [4, 2, 0] => [2, 1, 0] (d.m.y)
+                 [0, 6, 4] => [0, 2, 1] (y.d.m)
+                 */
+
             }
         }
     }
