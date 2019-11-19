b8721b253cc4d61637056bd99c761a3d916eebe0
Gregor Kofler <info@gregorkofler.com>
2019-11-19 14:33:20 +0100

    HTML head section (partially) moved to separate template snippets

--- view/admin/layout_with_menu.php
+++ view/admin/layout_with_menu.php
@@ -2,27 +2,7 @@
 
 <html>
 	<head>
-
-		<title>vxWeb Admin</title>
-		<meta name='keywords' content=''>
-		<meta name='description' content=''>
-		<meta name='robots' content='noindex, nofollow'>
-
-		<meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-		<meta name='author' content='Gregor Kofler - Mediendesign und Webapplikationen, http://gregorkofler.com'>
-
-		<link rel='icon' type='image/x-icon' href='/favicon.ico'>
-		<link type='text/css' rel='stylesheet' href="<?= \vxPHP\Application\Application::getInstance()->asset('css/admin.min.css') ?>">
-        <script type="text/javascript" src="<?= \vxPHP\Application\Application::getInstance()->asset('js/admin/vxjs.js') ?>"></script>
-        <!--
-        <script type="text/javascript" src="/js/vendor/vxJS/core.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/xhr.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widget.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widgets/xhrform.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widgets/sortable.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widgets/simpletabs.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widgets/calendar.js"></script>
-        -->
+        <?= $this->includeFile('admin/snippets/html_head.php') ?>
 
         <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
--- view/admin/layout_without_menu.php
+++ view/admin/layout_without_menu.php
@@ -2,23 +2,8 @@
 
 <html>
 	<head>
+        <?= $this->includeFile('admin/snippets/html_head.php') ?>
 
-		<title>vxWeb Admin</title>
-		<meta name='keywords' content=''>
-		<meta name='description' content=''>
-		<meta name='robots' content='noindex, nofollow'>
-
-		<meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-		<meta name='author' content='Gregor Kofler - Mediendesign und Webapplikationen, http://gregorkofler.com'>
-		<link rel='icon' type='image/x-icon'	href='/favicon.ico'>
-		<link type='text/css' rel='stylesheet'	href='<?= \vxPHP\Application\Application::getInstance()->asset('css/admin.min.css') ?>'>
-        <script type="text/javascript" src="<?= \vxPHP\Application\Application::getInstance()->asset('js/admin/vxjs.js') ?>"></script>
-        <!--
-        <script type="text/javascript" src="/js/vendor/vxJS/core.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/xhr.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widget.js"></script>
-        <script type="text/javascript" src="/js/vendor/vxJS/widgets/xhrform.js"></script>
-        -->
         <script type="text/javascript">
             if(!this.vxWeb) {
                 this.vxWeb = {};
