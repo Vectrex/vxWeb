81c995195eb6ad13f98a16995caca94d920f01c8
Gregor Kofler <info@gregorkofler.com>
2019-11-19 14:41:43 +0100

    Typo

--- view/admin/snippets/html_head.php
+++ view/admin/snippets/html_head.php
@@ -4,7 +4,7 @@
 <meta name='robots' content='noindex, nofollow'>
 <meta http-equiv='content-type' content='text/html; charset=UTF-8'>
 <meta name='web-author' content='Gregor Kofler - Mediendesign und Webapplikationen, http://gregorkofler.com'>
-<meta name="csrf-token" content="<?= (new \vxPHP\Security\Csrf\CsrfTokenManager())->refreshToken('admin') ?>"
+<meta name="csrf-token" content="<?= (new \vxPHP\Security\Csrf\CsrfTokenManager())->refreshToken('admin') ?>">
 <link rel='icon' type='image/x-icon' href='/favicon.ico'>
 <link type='text/css' rel='stylesheet' href='<?= \vxPHP\Application\Application::getInstance()->asset('css/admin.min.css') ?>'>
 <script type="text/javascript" src="<?= \vxPHP\Application\Application::getInstance()->asset('js/admin/vxjs.js') ?>"></script>
