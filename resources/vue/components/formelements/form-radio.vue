7bd73d5bf098c4a1960ba4a4930887c310632f94
Gregor <info@gregorkofler.com>
2020-08-06 01:08:15 +0200

    feat: Added a form-radio component

---
+++ resources/vue/components/formelements/form-radio.vue
@@ -0,0 +1,17 @@
+<template>
+  <label class="form-radio" v-for="option in options" v-bind="$attrs">
+    <input
+        @change="$emit('input', $event.target.value)"
+        :checked="value"
+        :value="option.key || option.label || option"
+        type="radio"
+    >
+    <i class="form-icon"></i> {{ option.label || option }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {options: Array, value: [String, Number]}
+}
+</script>
