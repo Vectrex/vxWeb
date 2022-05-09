5a62176039cde4ea42444b9949c891eafdd1000d
Gregor Kofler <info@gregorkofler.com>
2022-05-09 22:44:31 +0200

    Initial commit for Vite backend integration

---
+++ resources/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: './src/main.js'
+    }
+  }
+})
--- view/admin/layout_without_menu.php
+++ view/admin/layout_without_menu.php
@@ -3,6 +3,7 @@
 <html>
 	<head>
         <?= $this->includeFile('admin/snippets/html_head.php') ?>
+        <script type="module" src="http://localhost:3000/src/main.js"></script>
     </head>
 
 	<body>
