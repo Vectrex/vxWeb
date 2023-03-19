6b69836aba9c59003fe97f406a2da1363bdb6820
Gregor Kofler <info@gregorkofler.com>
2023-03-19 17:12:53 +0100

    Import tiptap extensions explicitly; improve tiptap component; replace checkbox with switch in article form

--- resources/src/components/misc/tiptap.vue
+++ resources/src/components/misc/tiptap.vue
@@ -1,13 +1,25 @@
 <script setup>
   import { Editor, EditorContent } from '@tiptap/vue-3'
-  import StarterKit from '@tiptap/starter-kit'
+  import Document from '@tiptap/extension-document'
+  import Text from '@tiptap/extension-text'
+  import Bold from '@tiptap/extension-bold'
+  import Italic from '@tiptap/extension-italic'
+  import ListItem from '@tiptap/extension-list-item'
+  import BulletList from '@tiptap/extension-bullet-list'
+  import OrderedList from '@tiptap/extension-ordered-list'
+  import Paragraph from '@tiptap/extension-paragraph'
+  import HardBreak from '@tiptap/extension-hard-break'
+  import History from '@tiptap/extension-history'
+  import { ArrowUturnLeftIcon, ArrowUturnRightIcon } from '@heroicons/vue/24/solid'
 </script>
 
 <template>
   <div>
     <div class="flex space-x-1 items-center justify-start rounded-t p-1 bg-slate-200">
-      <button @click="editor.chain().focus().toggleBold().run()" :class="['icon-link font-bold w-12', { 'bg-slate-400': editor?.isActive('bold') }]">Ab</button>
-      <button @click="editor.chain().focus().toggleItalic().run()" :class="['icon-link italic w-12', { 'bg-slate-400': editor?.isActive('italic') }]">Ab</button>
+      <button @click="editor.chain().focus().toggleBold().run()" :class="buttonClass('bold')"><strong>Ab</strong></button>
+      <button @click="editor.chain().focus().toggleItalic().run()" :class="buttonClass('italic')"><em>Ab</em></button>
+      <button @click="editor.commands.undo()" :class="buttonClass()"><arrow-uturn-left-icon class="h-5 w-5" /></button>
+      <button @click="editor.commands.redo()" :class="buttonClass()"><arrow-uturn-right-icon class="h-5 w-5" /></button>
     </div>
     <editor-content :editor="editor" class="border-slate-300 border-2 p-2" />
   </div>
@@ -22,7 +34,7 @@ export default {
   emits: ['update:modelValue'],
   data() {
     return {
-      editor: null,
+      editor: null
     }
   },
   watch: {
@@ -35,7 +47,16 @@ export default {
   mounted () {
     this.editor = new Editor({
       extensions: [
-        StarterKit,
+        Document,
+        Text,
+        Paragraph,
+        HardBreak,
+        History,
+        BulletList,
+        OrderedList,
+        ListItem,
+        Bold,
+        Italic
       ],
       content: this.modelValue,
       onUpdate: () => this.$emit('update:modelValue', this.editor.getHTML())
@@ -44,5 +65,10 @@ export default {
   beforeUnmount() {
     this.editor.destroy()
   },
+  methods: {
+    buttonClass (isActive) {
+      return 'icon-link' + (isActive && this.editor?.isActive(isActive) ? ' bg-slate-500' : '');
+    }
+  }
 }
 </script>
--- resources/src/components/views/articles/ArticleForm.vue
+++ resources/src/components/views/articles/ArticleForm.vue
@@ -1,6 +1,7 @@
 <script setup>
   import DatePicker from "@/components/vx-vue/datepicker.vue";
   import FormSelect from "@/components/vx-vue/form-select.vue";
+  import FormSwitch from "@/components/vx-vue/form-switch.vue";
   import Tiptap from "@/components/misc/tiptap.vue";
   import SubmitButton from "@/components/app/SubmitButton.vue";
   import DateFunctions from "@/util/date-functions";
@@ -18,16 +19,6 @@
           :type="element.type"
           class="form-input w-full"
           v-bind="element.attrs"
-        />
-        <input
-            v-else-if="element.type === 'checkbox'"
-            :id="element.model"
-            v-model="form[element.model]"
-            :type="element.type"
-            class="form-checkbox ml-2"
-            v-bind="element.attrs"
-            :true-value="true"
-            :false-value="false"
         />
         <textarea
             v-else-if="element.type === 'textarea'"
@@ -52,7 +43,7 @@
 
 <script>
 export default {
-  components: { datepicker: DatePicker, formSelect: FormSelect, tiptap: Tiptap },
+  components: { datepicker: DatePicker, formSelect: FormSelect, formSwitch: FormSwitch, tiptap: Tiptap },
   name: "ArticleForm",
   inject: ['api'],
   props: ['id'],
@@ -78,7 +69,7 @@ export default {
         { type: 'datepicker', model: 'article_date', label: 'Artikeldatum', attrs: datepickerAttrs },
         { type: 'datepicker', model: 'display_from', label: 'Anzeige von', attrs: { ...datepickerAttrs, validFrom: new Date() }},
         { type: 'datepicker', model: 'display_until', label: 'Anzeige bis', attrs: {...datepickerAttrs, validFrom: new Date()}},
-        { type: 'checkbox', model: 'customflags', label: 'Markiert' },
+        { type: 'form-switch', model: 'customflags', label: 'Markiert', attrs: { 'class': 'ml-2' } },
         { type: 'form-select', model: 'articlecategoriesid', label: 'Kategorie', required: true, options: this.categories, attrs: { 'class': 'w-full' } },
         { type: 'text', model: 'headline', label: 'Überschrift/Titel', required: true },
         { type: 'text', model: 'subline', label: 'Unterüberschrift' },
