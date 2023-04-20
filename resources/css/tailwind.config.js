fc3821b1ed25fa248f4ec02ce0e8957124e4401c
Gregor Kofler <info@gregorkofler.com>
2023-04-21 00:49:06 +0200

    Preparatory work to adapt installer to Tailwind CSS

--- resources/css/tailwind.config.js
+++ resources/css/tailwind.config.js
@@ -1,9 +1,43 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['../../view/installer/*'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'vxvue': {
+          DEFAULT: 'var(--vxvue-color)',
+          '50': 'var(--vxvue-color-50)',
+          '100': 'var(--vxvue-color-100)',
+          '200': 'var(--vxvue-color-200)',
+          '300': 'var(--vxvue-color-300)',
+          '400': 'var(--vxvue-color-400)',
+          '500': 'var(--vxvue-color-500)',
+          '600': 'var(--vxvue-color-600)',
+          '700': 'var(--vxvue-color-700)',
+          '800': 'var(--vxvue-color-800)',
+          '900': 'var(--vxvue-color-900)',
+        },
+        'vxvue-alt': {
+          DEFAULT: 'var(--vxvue-highlight)',
+          '50': 'var(--vxvue-highlight-50)',
+          '100': 'var(--vxvue-highlight-100)',
+          '200': 'var(--vxvue-highlight-200)',
+          '300': 'var(--vxvue-highlight-300)',
+          '400': 'var(--vxvue-highlight-400)',
+          '500': 'var(--vxvue-highlight-500)',
+          '600': 'var(--vxvue-highlight-600)',
+          '700': 'var(--vxvue-highlight-700)',
+          '800': 'var(--vxvue-highlight-800)',
+          '900': 'var(--vxvue-highlight-900)',
+        },
+        success: 'var(--success-color)',
+        error:  'var(--error-color)',
+        warn:  'var(--warn-color)',
+        info:  'var(--info-color)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
--- view/installer/installer.php
+++ view/installer/installer.php
@@ -15,24 +15,39 @@
 		<link type='text/css' rel='stylesheet' href='<?= \vxPHP\Application\Application::getInstance()->asset('style.css') ?>'>
     </head>
 
-	<body>
+	<body class="min-h-screen w-full lg:w-1/2 lg:mx-auto px-1 py-4">
+		<div>
+            <h1 class="text-3xl text-vxvue">vxWeb Installer</h1>
 
-		<div id="page" class="container grid-md">
-            <h1>vxWeb Installer</h1>
-            <div class="divider text-center" data-content="Schreibrechte für Verzeichnisse"></div>
-            <table class="table table-striped">
-                <?php foreach($this->path_checks as $path => $check): ?>
-                <tr>
-                    <td>Ordner <?= $path ?> beschreibbar</td><td><?= $check['writable']  ? '<span class="label label-success">ja</span>' : '<span class="label label-error">nein</span>' ?></td>
-                </tr>
-                <?php endforeach; ?>
-            </table>
+            <div class="relative">
+                <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                    <div class="w-full border-t border-gray-300"></div>
+                </div>
+                <div class="relative flex justify-center">
+                    <span class="bg-white px-2 text-sm text-gray-500">Schreibrechte für Verzeichnisse</span>
+                </div>
+            </div>
+
+            <?php foreach($this->path_checks as $path => $check): ?>
+            <div class="flex justify-between items-center py-2">
+                <span>Ordner <pre class="inline-block text-vxvue-alt-600 font-bold"><?= $path ?></pre> beschreibbar</span>
+                <span class="w-20 font-bold text-white text-center p-2 rounded <?= $check['writable']  ? 'bg-green-600' : 'bg-red-600' ?>">
+                    <?= $check['writable']  ? 'ja' : 'nein' ?>
+                </span>
+            </div>
+            <?php endforeach; ?>
 
             <?php if(!$this->paths_ok): ?>
 
-                <div class="toast toast-error">
-                    Erlaube zunächst Schreibzugriff auf die derzeit nicht beschreibbaren Ordner und die darin enthaltenen Dateien.<br>
-                    Im darauf folgenden Schritt wird dann die Datenbank eingerichtet.
+                <div class="border-l-4 border-red-600 bg-red-50 p-4 rounded">
+                    <div class="flex">
+                        <div class="ml-3">
+                            <p class="text-sm text-red-800">
+                                Erlaube zunächst Schreibzugriff auf die derzeit nicht beschreibbaren Ordner und die darin enthaltenen Dateien.<br>
+                                Im darauf folgenden Schritt wird dann die Datenbank eingerichtet.
+                            </p>
+                        </div>
+                    </div>
                 </div>
 
             <?php elseif($this->success): ?>
@@ -63,7 +78,14 @@
 
             <?php else: ?>
 
-                <div class="divider text-center" data-content="Datenbank Einstellungen"></div>
+                <div class="relative">
+                    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                        <div class="w-full border-t border-gray-300"></div>
+                    </div>
+                    <div class="relative flex justify-center">
+                        <span class="bg-white px-2 text-sm text-gray-500">Datenbank Einstellungen</span>
+                    </div>
+                </div>
 
                 <?php if($this->connection_error): ?>
                     <div class="toast toast-error"><?= $this->connection_error ?></div>
@@ -74,7 +96,40 @@
                 <?php endif; ?>
 
                 <?php if($this->db_settings_form): ?>
-                    <?= $this->db_settings_form ?>
+                <div class="py-2 space-y-2">
+                    <div class="rounded px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-vxvue-500">
+                        <label for="form-host" class="block text-xs font-medium text-gray-900">Host</label>
+                        <input type="text" id="form-host" class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="localhost">
+                    </div>
+                    <div class="rounded px-3 pb-1.5 pt-2.5 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-vxvue-500">
+                        <label for="form-port" class="block text-xs font-medium text-gray-900">Port</label>
+                        <input type="text" id="form-port" class="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="3306">
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label{error_port: text-error}">Port</label>
+                        {input:port {"maxlength": "5", "class": "form-input"} }
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label{error_dbname: text-error}">Name der Datenbank</label>
+                        {input:dbname {"maxlength": "128", "class": "form-input"} }
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label{error_user: text-error}">User</label>
+                        {input:user {"maxlength": "128", "class": "form-input"} }
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label{error_password: text-error}">Passwort</label>
+                        {input:password {"maxlength": "128", "class": "form-input"} }
+                    </div>
+                    <div class="form-group">
+                        <label class="form-label{error_db_type: text-error}">Typ</label>
+                        {dropdown:db_type {"class": "form-select"} }
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success" onclick="this.disabled = true;">Übernehmen</button>
+                    </div>
+                </div>
                 <?php endif; ?>
 
             <?php endif; ?>
