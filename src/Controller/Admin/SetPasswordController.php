e3f8137729ebd04e3da75edd2f1f95851a76fabe
gregor <gregor@mail.agindo.intern>
2023-10-24 17:04:20 +0200

    Added controller for setting a new password

---
+++ src/Controller/Admin/SetPasswordController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use vxPHP\Application\Application;
+use vxPHP\Controller\Controller;
+use vxPHP\Http\JsonResponse;
+use vxPHP\Http\ParameterBag;
+use vxPHP\Http\Response;
+use vxPHP\Security\Password\PasswordEncrypter;
+
+class SetPasswordController extends Controller
+{
+    protected function execute (): JsonResponse
+    {
+        $hash = $this->route->getPathParameter('auth');
+        $bag = new ParameterBag(json_decode($this->request->getContent(), true, 512, JSON_THROW_ON_ERROR));
+
+        $pwd = $bag->get('password');
+        $pwdRepeat = $bag->get('passwordRepeat');
+        if (!$pwd || $pwd !== $pwdRepeat) {
+            return new JsonResponse(['success' => false, 'message' => 'Ungültiges Passwort.']);
+        }
+        $pdo = Application::getInstance()->getVxPDO();
+        $row = $pdo->doPreparedQuery("SELECT adminid FROM admin WHERE temporary_hash = ?", [$hash])->current();
+        if (!$row) {
+            return new JsonResponse(['success' => false, 'message' => 'Ungültiger Code.'], Response::HTTP_NOT_FOUND);
+        }
+        $pdo->updateRecord('admin', $row['adminid'], [
+            'pwd' => (new PasswordEncrypter())->hashPassword($pwd),
+            'temporary_hash' => null
+        ]);
+        return new JsonResponse(['success' => true, 'message' => 'Passwort aktualisiert.']);
+    }
+}
